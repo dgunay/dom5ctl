4a7d4831a2e4de8e563bae90e98021173df74924
Devin Gunay <devingunay@gmail.com>
2021-07-16 02:33:08 +0000

    updates

--- run_dominions.py
+++ run_dominions.py
@@ -1,17 +1,33 @@
 #!/usr/bin/env python3
 
 import glob
-from os.path import expanduser
+from os.path import expanduser, basename
 home = expanduser("~")
 
 # Edit this script with the game settings you want.
 
-start_script = "~/.steam/steamapps/common/Dominions5/dom5.sh"
+start_script = "{}/.steam/steamapps/common/Dominions5/dom5.sh".format(home)
 args = [
+    "--nosteam",
     "--tcpserver",
     "--textonly",
+    "--mapfile steve.map",
+    "--port 1024",
+    "--renaming",
+    "--thrones 2 4 0",
+    "--requiredap 6",        
     # Put other settings here
 ]
 
-modfiles = glob("{}/.dominions5/mods/*.dm")
-print(modfiles)
+modfiles = list(map(basename, glob.glob("{}/.dominions5/mods/*.dm".format(home))))
+for modfile in modfiles:
+    args.append('--enablemod')
+    args.append(modfile)
+
+command = start_script
+sep = " "
+for arg in args:
+    command += sep + arg
+
+import os
+os.system(command)
