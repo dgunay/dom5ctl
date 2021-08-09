38276371ddd8a6c58250088e965527963bcd3f94
Devin Gunay <devingunay@gmail.com>
2021-08-09 03:17:07 +0000

    turn timer script

---
+++ change_timer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+# Changes the turn timer of a given game.
+
+import glob
+from os.path import expanduser, basename, exists
+import sys
+import tempfile
+import shutil
+home = expanduser("~")
+
+# What game?
+if len(sys.argv) < 3:
+   raise Exception("Usage: <game name> <minutes>")
+
+game = sys.argv[1]
+minutes = int(sys.argv[2])
+
+# Place a text file in the game folder
+game_folder = "{}/.dominions5/savedgames/{}".format(home, game)
+if not exists(game_folder):
+   raise Exception("Game {} does not exist".format(game))
+
+fp = tempfile.NamedTemporaryFile(prefix="dominions", suffix="sucks")
+fp.write(b"setinterval %d" % minutes)
+
+shutil.copy(fp.name, "{}/domcmd".format(game_folder))
