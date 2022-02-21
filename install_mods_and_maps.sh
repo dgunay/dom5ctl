710477cdbda0094961e5929a0eef79518ad9f105
Devin Gunay <devingunay@gmail.com>
2022-02-21 01:52:51 +0000

    clear mods folder first

--- install_mods_and_maps.sh
+++ install_mods_and_maps.sh
@@ -5,6 +5,9 @@ set -e
 mkdir -p $HOME/.dominions5/maps
 mkdir -p $HOME/.dominions5/mods
 
+# clear mods folder
+rm -rf $HOME/.dominions/mods/*
+
 cp -r ./maps/* $HOME/.dominions5/maps
 cp -r ./mods/* $HOME/.dominions5/mods
 rm -rf $HOME/.dominions5/mods/*.zip
